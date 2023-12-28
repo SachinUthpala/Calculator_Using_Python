0d069427146d1455bc70d154e6f37d67b7aecf9a
Sachin Uthpala <140322238+SachinUthpala@users.noreply.github.com>
2023-12-28 06:58:29 +0530

    Added Display Lables

--- cal.py
+++ cal.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
-import cal
-
+LIGTH_GRAY = "#F5F5F5"
+LABLE_COLOR = "#25265E"
+SMALL_TEXT = ("Arial", 16)
+LARGE_FONT = ("Arial" , 40 , "bold")
 
 class Calculator:
     def __init__(self):
@@ -9,6 +11,36 @@ class Calculator:
         self.window.geometry("375x667")
         self.window.resizable(0,0)
         self.window.title("CalBySachin")
+
+        self.total_expression = "0"
+        self.current_expression = "0"
+
+        self.display_frame = self.create_display_frame()
+        
+        self.total_lable , self.lable = self.create_display_lables()
+
+        self.button_frame = self.create_button_frame()
+
+    def create_display_lables(self):
+        total_lable = tk.Label(self.display_frame, text= self.total_expression, anchor=tk.E,
+                               bg=LIGTH_GRAY, fg=LABLE_COLOR, padx=24, font= SMALL_TEXT )
+        total_lable.pack(expand=True, fill="both" )
+
+        lable = tk.Label(self.display_frame, text= self.current_expression, anchor=tk.E,
+                               bg=LIGTH_GRAY, fg=LABLE_COLOR, padx=24, font=LARGE_FONT )
+        lable.pack(expand=True, fill="both" )
+
+        return total_lable,lable
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGTH_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_button_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill="both")
+        return frame
 
     def run(self):
         self.window.mainloop()
